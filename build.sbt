ff4ffe680219cc003b299a92589ea6cb700be4a4
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-08-05 16:13:03 +0200

    inital commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "PokemonInScala"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/game.scala
@@ -0,0 +1,5 @@
+object game {
+  def main(args: Array[String]): Unit ={
+    println("Welcome to Monster RPG!")
+  }
+}
