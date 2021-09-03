409044599213ccf707b2ae6d68e0fe9f832f0df6
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-09-03 12:21:06 +0200

    added LifePoints to Status

--- src/main/scala/gameLogic/Status.scala
+++ src/main/scala/gameLogic/Status.scala
@@ -1,3 +1,3 @@
 package gameLogic
 
-case class Status(atk: Int = 50, dfn: Int = 50, mAtk: Int = 50, mDfn: Int = 50, init: Int = 50)
+case class Status(LP:Int=50, atk: Int = 50, dfn: Int = 50, mAtk: Int = 50, mDfn: Int = 50, init: Int = 50)
