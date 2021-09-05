8744de4b072e22b39f448692f0052f66c5cea112
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-09-05 10:03:27 +0200

    added MoveTest

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "MonsterRPG_inScala"
 version := "0.1"
 
 scalaVersion := "2.13.6"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.9"
+libraryDependencies +=
+  "org.scalatest" %% "scalatest" % "3.2.9" % Test
---
+++ src/test/scala/MoveTest.scala
@@ -0,0 +1,25 @@
+import gameLogic.Move
+import org.scalatest.wordspec.AnyWordSpec
+
+class MoveTest extends AnyWordSpec{
+  val move: Move = Move("moveName", 20)
+  "A Move" must {
+    "have a name" in {
+      assert(move.name.isInstanceOf[String])
+      assert(move.name.nonEmpty)
+      assert(move.name=="moveName")
+    }
+    "have an attack strength" in{
+      assert(move.attack.isInstanceOf[Int])
+      assert(move.attack >= 0)
+      // throw exception if attack is bellow 0,
+      // healing should be handled with effect
+    }
+  }
+  "A Move" should{
+    "have a nice stringrepresentation" in{
+      assert(move.toString.isInstanceOf[String])
+      assert(move.toString == "moveName \nattack: 20")
+    }
+  }
+}
