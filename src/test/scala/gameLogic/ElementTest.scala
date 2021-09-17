c3754e0a2d54e09def66a8815d835f1de1187219
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-09-17 13:06:17 +0200

    added test to ElementTest

--- src/test/scala/gameLogic/ElementTest.scala
+++ src/test/scala/gameLogic/ElementTest.scala
@@ -3,9 +3,11 @@ package gameLogic
 import org.scalatest.wordspec.AnyWordSpec
 
 class ElementTest extends AnyWordSpec{
+  val neutral: Element = Element("neutral")
   "A Element" should{
     "have a name" in{
-      //assert(Element("neutral").element.isInstanceOf[String])
+      assert(neutral.name.isInstanceOf[String])
+      assert(neutral.name == neutral.toString)
     }
   }
 }
